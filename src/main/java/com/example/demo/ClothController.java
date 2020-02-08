3fce7b7ac9ce3d96bdc98d10fb8359fd05289ba9
Rafal Burczaniuk <kielbi12@gmail.com>
2020-02-08 18:04:48 +0100

    Little change in exception

--- src/main/java/com/example/demo/ClothController.java
+++ src/main/java/com/example/demo/ClothController.java
@@ -1,19 +1,27 @@
 package com.example.demo;
 
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.sun.tools.sjavac.Util.getStackTrace;
+
 @Slf4j
 @RestController
 public class ClothController {
 
     @Autowired
     ClothService cs;
+
 
     @GetMapping("/all")
     public Hashtable<String, Cloth> getClothes() {
@@ -27,11 +35,13 @@ public class ClothController {
             AtomicInteger count = new AtomicInteger(cloth.getViewQuantity());
             cloth.setViewQuantity(count.incrementAndGet());
             return cloth;
-        } catch (NullPointerException n) {
-            String info = "Nie ma takiego obiektu";
-            log.info(info);
+        } catch (Exception ex) {
+            Gson gson = new Gson();
+            Map<String, String> exc_map = new HashMap<String, String>();
+            exc_map.put("message", ex.toString());
+            log.info(gson.toJson(exc_map));
+            return null;
         }
-        return new Cloth();
     }
 
 
